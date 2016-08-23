cf3319f710c614171f16caf669b7a41a8750922c
Steve Lee <steve.lee@fingerprints.com>
2016-08-23 12:14:03 +0900

    Fix  Main Menu
    Change-Id: Ib42a98eaf588e1fab1715069be1a619b7e29a46b

--- src/com/jny/Main.java
+++ src/com/jny/Main.java
@@ -8,33 +8,32 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
 	// write your code here
-        final Process clear = Runtime.getRuntime().exec("clear");
-        System.out.print("*** MENU ***");
-        System.out.print("1. INSERT");
-        System.out.print("2. PRINT");
-        System.out.print("3. SORT");
-        System.out.print("4. END");
-        System.out.print("choice : ");
+        while(true) {
+            final Process clear = Runtime.getRuntime().exec("clear");
+            System.out.print("*** MENU ***\n");
+            System.out.print("1. INSERT\n");
+            System.out.print("2. PRINT\n");
+            System.out.print("3. SORT\n");
+            System.out.print("4. END\n");
+            System.out.print("choice : ");
 
-        String menuNum;
-        Scanner scanIn = new Scanner(System.in);
-        menuNum = scanIn.nextLine();
+            String menuNum;
+            Scanner scanIn = new Scanner(System.in);
+            menuNum = scanIn.nextLine();
 
-        switch(Integer.parseInt(menuNum))
-        {
-            case 1 :
-                //insert(); break;
-                break;
-            case 2 : //print(); break;
-                break;
-            case 3 : //sort(); break;
-                break;
-            case 4 : //exit(1);
-                break;
-            default:
+            switch (Integer.parseInt(menuNum)) {
+                case 1:
+                    //insert(); break;
+                    break;
+                case 2: //print(); break;
+                    break;
+                case 3: //sort(); break;
+                    break;
+                case 4: //exit(1);
+                    break;
+                default:
+            }
         }
-
-    }
 
     }
 }
