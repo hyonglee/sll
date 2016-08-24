84be2e2fe267d577d0cb74cdc4fba8fddf01ecb8
Steve Lee <steve.lee@fingerprints.com>
2016-08-24 12:18:32 +0900

    Implement printNode() function
    Change-Id: I796871ec16be5dffe150d73079ba43ef1225f926

--- src/com/jny/Main.java
+++ src/com/jny/Main.java
@@ -5,9 +5,36 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    static void insertNode(LinkedList simpleLinkedList){
+        System.out.print("Enter Integer: \n");
+        Scanner scanIn = new Scanner(System.in);
+        int data = scanIn.nextInt();
+        ListNode newNode = new ListNode(data);
+        simpleLinkedList.insertAtEnd(newNode);
+    }
+    static void printNode(LinkedList simpleLinkedList){
+        ListNode currentNode;
 
-	// write your code here
+        if(simpleLinkedList.head == null){
+            System.out.print("Empty List\n");
+            return;
+        }
+        System.out.print("\n<PRINT>\n");
+        currentNode = simpleLinkedList.head;
+        while(currentNode.next != null)
+        {
+            System.out.print(currentNode.data+" -> ");
+            currentNode = currentNode.next;
+        }
+        System.out.print(currentNode.data+"\n");
+    }
+    static void sortNode(LinkedList simpleLinkedList){
+
+    }
+    public static void main(String[] args) throws IOException {
+        LinkedList simpleLinkedList ;
+        simpleLinkedList = new LinkedList();
+
         while(true) {
             final Process clear = Runtime.getRuntime().exec("clear");
             System.out.print("*** MENU ***\n");
@@ -19,23 +46,19 @@ public class Main {
 
             Scanner scanIn = new Scanner(System.in);
             int menuNum = scanIn.nextInt();
-            LinkedList simpleLinkedList ;
-            simpleLinkedList = new LinkedList();
 
             switch (menuNum) {
                 case 1:
-                    System.out.print("Enter Integer: \n");
-                    int data = scanIn.nextInt();
-                    ListNode newNode = new ListNode(data);
-                    simpleLinkedList.insertAtEnd(newNode);
+                    insertNode(simpleLinkedList);
                     break;
-                case 2: //print(); break;
-
+                case 2:
+                    printNode(simpleLinkedList);
                     break;
-                case 3: //sort(); break;
+                case 3:
+                    sortNode(simpleLinkedList);
                     break;
-                case 4: //exit(1);
-                    break;
+                case 4:
+                    return;
                 default:
             }
         }
